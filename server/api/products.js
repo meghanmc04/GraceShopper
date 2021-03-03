3100058f5964f5fb1922707186a1998e01b3d81d
Myra Khan <trueimpulse@yahoo.com>
2021-03-03 16:44:19 -0500

    feat(single-product): express route for a single product

--- server/api/products.js
+++ server/api/products.js
@@ -1,3 +1,13 @@
 const router = require('express').Router()
 const {Product} = require('../db/models')
 module.exports = router
+
+//GET /api/products/:id
+router.get('/:id', async (req, res, next) => {
+  try {
+    const product = await Product.findByPk(req.params.id)
+    res.send(product)
+  } catch (err) {
+    next(err)
+  }
+})
