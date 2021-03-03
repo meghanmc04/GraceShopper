2258ac7add4c037cc08ec062c4b865ebef49f36c
Myra Khan <trueimpulse@yahoo.com>
2021-03-03 17:46:39 -0500

    feat(single-product): thunk created for single product

---
+++ client/components/SingleProduct.js
@@ -0,0 +1,16 @@
+// import React from 'react'
+// import {getProduct} from '../store/singleProduct'
+// import {connect} from 'react-redux'
+
+// class SingleProduct extends React.Component {
+//   async componentDidMount() {
+//     await this.props.getProduct()
+//   }
+
+//   render() {
+//     const product = this.props.product;
+//     return(
+
+//     )
+//   }
+// }
--- client/store/index.js
+++ client/store/index.js
@@ -3,8 +3,12 @@ import {createLogger} from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
+import singleProductReducer from './singleProduct'
 
-const reducer = combineReducers({user})
+const reducer = combineReducers({
+  user,
+  singleProduct: singleProductReducer
+})
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
