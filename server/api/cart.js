7c74c389ec33d1bf2b1936d98555564ca0ac6db1
Ana Judd <anajudd2015@gmail.com>
2021-03-04 13:24:22 -0600

    feat(cart): Added some tests for cart routes

--- server/api/cart.js
+++ server/api/cart.js
@@ -31,9 +31,10 @@ router.post('/', async (req, res, next) => {
 })
 
 // PUT /api/cart/:id
+// I feel like I can rewrite this to be more concise...
 router.put('/:id', async (req, res, next) => {
   try {
-    const currentCart = await Cart.findByPk(req.params.id)
+    const currentCart = await Cart.findByPk(req.params.id, {include: Product})
     if (await currentCart.containsProduct(Number(req.body.id))) {
       const productInCart = await CartProducts.findOne({
         where: {
@@ -46,8 +47,9 @@ router.put('/:id', async (req, res, next) => {
     } else {
       await currentCart.addProduct(req.body.id)
     }
-    await currentCart.save()
-    res.send(currentCart)
+    const updatedCart = await Cart.findByPk(req.params.id, {include: Product})
+    await updatedCart.save()
+    res.send(updatedCart)
   } catch (error) {
     console.log('error in the PUT /api/cart/:id route', error)
     next(error)
