030122c0c9ff733e60c93a7d4a69eef4202deae2
Marjorie Mejia <mm035359@gmail.com>
2021-03-04 12:33:02 -0500

    field(component): AllComponets edited to include link

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -3,7 +3,7 @@ import {connect} from 'react-redux'
 //fetch thunk
 import {getAllProducts} from '../store/allProducts'
 //not sure if we will need a link here
-//import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
 export class AllProducts extends React.Component {
   //getting our info as props from store?
@@ -23,17 +23,18 @@ export class AllProducts extends React.Component {
         {/* our map of jeans will go here  */}
         {products.map(product => (
           <div key={product.id}>
-            <img
-              src={product.imageUrl}
-              alt={product.name}
-              style={{width: '200px'}}
-            />
+            <Link to={`/products/${product.id}`}>
+              <img
+                src={product.imageUrl}
+                alt={product.name}
+                style={{width: '200px'}}
+              />
 
-            <h5>
-              Style: {product.color} {product.cut}
-            </h5>
+              <h5>
+                Style: {product.color} {product.cut}
+              </h5>
+            </Link>
           </div>
-          // do we need a link to take us to single pair of jeans
         ))}
       </div>
     )
