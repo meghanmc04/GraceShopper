3b8390c81c95d3dd4e23e98c8eba6eae723b34d9
Ana Judd <anajudd2015@gmail.com>
2021-03-04 20:52:46 -0600

    feat(cart):Added frontend component and reducer

--- client/components/cart.js
+++ client/components/cart.js
@@ -1,6 +1,74 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {
+  getCart,
+  getCartWithItemRemoved,
+  getCartWithItemAdded
+} from '../store/cart'
 
-const Cart = props => {}
+// get userId from state through mapStateToProps and update thunk to incorporate userId
+class Cart extends React.Component {
+  constructor() {
+    super()
+    this.addAnotherToCart = this.addAnotherToCart.bind(this)
+    this.removeOneFromCart = this.removeOneFromCart.bind(this)
+  }
+  componentDidMount() {
+    // hardcoding cartId for now until updated with userId from props/ route
+    this.props.getCart(1)
+  }
+  addAnotherToCart(cartId, productId) {
+    this.props.getCartWithItemAdded(cartId, productId)
+  }
+  removeOneFromCart(cartId, productId) {
+    this.props.getCartWithItemRemoved(cartId, productId)
+  }
 
-export default connect()(Cart)
+  render() {
+    console.log('PROPS', this.props)
+    const products = this.props.cart.products
+    const cart = this.props.cart
+    return (
+      <div>
+        <h1>Cart Contents: </h1>
+        {products
+          ? products.map(product => (
+              <li key={product.id}>
+                {product.cut} {product.color} {product.size} <b>Price: </b>
+                {product.price} <b>Quantity in Cart: </b>
+                {product.cartProducts.quantity}
+                <button
+                  type="button"
+                  onClick={() => this.addAnotherToCart(cart.id, product.id)}
+                >
+                  +
+                </button>
+                <button
+                  type="button"
+                  onClick={() => this.removeOneFromCart(cart.id, product.id)}
+                >
+                  -
+                </button>
+              </li>
+            ))
+          : 'No products in cart'}
+        <h3>Subtotal: {this.props.cart.subTotal}</h3>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => ({
+  cart: state.cart
+})
+
+// Update this to userId once route has been updated!
+const mapDispatchToProps = dispatch => ({
+  getCart: cartId => dispatch(getCart(cartId)),
+  getCartWithItemRemoved: (cartId, productId) =>
+    dispatch(getCartWithItemRemoved(cartId, productId)),
+  getCartWithItemAdded: (cartId, productId) =>
+    dispatch(getCartWithItemAdded(cartId, productId))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Cart)
--- client/store/cart.js
+++ client/store/cart.js
@@ -2,10 +2,24 @@ import axios from 'axios'
 
 // ACTION TYPES
 const GOT_CART = 'GOT_CART'
+const GOT_ITEM_TO_ADD = 'GOT_ITEM_TO_ADD'
+const GOT_ITEM_TO_REMOVE = 'GOT_ITEM_TO_REMOVE'
 
 // ACTIONS CREATORS
 export const gotCart = cart => ({
   type: GOT_CART,
+  cart
+})
+
+// Question: If we are always returning a cart object could we use just one action type and one action creator???
+
+export const gotItemToAdd = cart => ({
+  type: GOT_ITEM_TO_ADD,
+  cart
+})
+
+export const gotItemToRemove = cart => ({
+  type: GOT_ITEM_TO_REMOVE,
   cart
 })
 
@@ -18,13 +32,46 @@ export const getCart = cartId => {
       const {data: cart} = await axios.get(`/api/cart/${cartId}`)
       dispatch(gotCart(cart))
     } catch (error) {
-      console.log('Error in the getcart thunk', error)
+      console.log('Error in the getCart thunk', error)
+    }
+  }
+}
+
+export const getCartWithItemAdded = (cartId, productId) => {
+  return async dispatch => {
+    try {
+      const {data: cart} = await axios.put(`/api/cart/${cartId}`, {
+        id: productId
+      })
+      dispatch(gotCart(cart))
+    } catch (error) {
+      console.log('Error in the getCartWithItemAdded thunk', error)
+    }
+  }
+}
+
+export const getCartWithItemRemoved = (cartId, productId) => {
+  return async dispatch => {
+    try {
+      const {data: cart} = await axios.put(`api/cart/removeItem/${cartId}`, {
+        id: productId
+      })
+      dispatch(gotCart(cart))
+    } catch (error) {
+      console.log('Error in the getCartWithItemRemved thunk', error)
     }
   }
 }
 
 // INITIAL STATE
 const intialState = {}
+
+// Or, should we divide state between subtotal, items, etc to make it easier to handle the data on the front end or are we okay with working with the entire cart object (which is pretty cumbersome)
+
+// const initialState = {
+//   subtotal: 0.00,
+//   products: []  <-- this we could possibly save as an object but we would have to re-work the data coming in a little
+// }
 
 // REDUCER
 function cartReducer(state = intialState, action) {
