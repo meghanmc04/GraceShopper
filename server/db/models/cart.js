10394076d2552a634a259f83a32d47a75e7620c8
Marjorie Mejia <mm035359@gmail.com>
2021-03-07 12:06:18 -0500

    feature(cart):added hook before bulk create changed validation for shopping cart

--- server/db/models/cart.js
+++ server/db/models/cart.js
@@ -4,22 +4,43 @@ const {CartProducts} = require('./cartProducts')
 
 const Cart = db.define('cart', {
   subTotal: {
-    type: Sequelize.DECIMAL(10, 2), // update
-    defaultValue: 0
+    type: Sequelize.INTEGER, // update
+    defaultValue: 0,
+    validate: {
+      min: 0
+    }
   },
   active: {
     type: Sequelize.BOOLEAN,
     defaultValue: false
   }
 })
+Cart.beforeBulkCreate(product => {
+  console.log('beforeBulkCreateProduct', product)
+  const add = Cart.addProducts(product)
+  console.log('add', add)
+  // for (const cart of carts) {
+  //   if (cart.subTotal) {
+  //     cart.subTotal += //procuct.price
+  //   }
+  // }
+
+  // //  updateOnDuplicate otherwise it won't be persisted
+  // if (
+  //   options.updateOnDuplicate &&
+  //   !options.updateOnDuplicate.includes('subTotal')
+  // ) {
+  //   options.updateOnDuplicate.push('subTotal')
+  // }
+})
 
 Cart.beforeSave(async cart => {
   const items = await cart.getProducts({include: CartProducts})
   let total = 0
   items.forEach(item => {
-    total += +item.price * item.cartProducts.quantity
+    total += item.price * item.cartProducts.quantity
   })
-  cart.subTotal = parseFloat(total).toFixed(2)
+  cart.subTotal = total / 100
   return cart
 })
 
